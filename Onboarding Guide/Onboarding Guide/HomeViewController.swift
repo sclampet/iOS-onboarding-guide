8668126ce06b112b27472320ba2aefbd736919f6
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-02 11:59:15 -0600

    create homeviewcontroller

---
+++ Onboarding Guide/Onboarding Guide/HomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewController.swift
+//  Onboarding Guide
+//
+//  Created by Scott Clampet on 9/2/19.
+//  Copyright © 2019 Scott Clampet. All rights reserved.
+//
+
+import UIKit
+
+private let cellId = "cellId"
+
+class HomeViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+
+    }
+
+    // MARK: UICollectionViewDataSource
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    
+        return cell
+    }
+
+}
