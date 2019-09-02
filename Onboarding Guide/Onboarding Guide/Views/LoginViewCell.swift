4840e71baeef619c4b220fcb1a14553d526953d7
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-02 16:28:00 -0600

    create loginviewcell

---
+++ Onboarding Guide/Onboarding Guide/Views/LoginViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewCell.swift
+//  Onboarding Guide
+//
+//  Created by Scott Clampet on 9/2/19.
+//  Copyright © 2019 Scott Clampet. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
