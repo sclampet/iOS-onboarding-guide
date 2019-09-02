aedd9c2226931f520e516e2aa24dd8f900a73748
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-02 11:54:52 -0600

    add addConstraintWithFormat helper extension

---
+++ Onboarding Guide/Onboarding Guide/Extensions/Helpers.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  Word Up
+//
+//  Created by Scott Clampet on 8/5/19.
+//  Copyright © 2019 Scott Clampet. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
