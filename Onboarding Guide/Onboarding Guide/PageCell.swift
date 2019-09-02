29714c9abef813df325d61c63ff01468ad9dce47
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-02 12:16:54 -0600

    create pagecell

---
+++ Onboarding Guide/Onboarding Guide/PageCell.swift
@@ -0,0 +1,20 @@
+//
+//  PageCell.swift
+//  Onboarding Guide
+//
+//  Created by Scott Clampet on 9/2/19.
+//  Copyright © 2019 Scott Clampet. All rights reserved.
+//
+
+import UIKit
+
+class PageCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
