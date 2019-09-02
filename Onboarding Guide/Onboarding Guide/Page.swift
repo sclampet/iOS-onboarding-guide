6c5c853f222c92551628c847767e324e082681ba
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-02 12:34:01 -0600

    create Page model

---
+++ Onboarding Guide/Onboarding Guide/Page.swift
@@ -0,0 +1,15 @@
+//
+//  Page.swift
+//  Onboarding Guide
+//
+//  Created by Scott Clampet on 9/2/19.
+//  Copyright © 2019 Scott Clampet. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    let imageName: String
+    let title: String
+    let bodyText: String
+}
