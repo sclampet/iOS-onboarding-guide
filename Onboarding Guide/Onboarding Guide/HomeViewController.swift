9c1855a359be2ab54fbd24124c305de078bdf432
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-02 12:11:50 -0600

    render 4 empty cells that scroll horizontally

--- Onboarding Guide/Onboarding Guide/HomeViewController.swift
+++ Onboarding Guide/Onboarding Guide/HomeViewController.swift
@@ -16,24 +16,33 @@ class HomeViewController: UICollectionViewController {
         super.viewDidLoad()
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
         collectionView.backgroundColor = .cyan
+        setupScrolling()
+    }
+    
+    func setupScrolling() {
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+        }
     }
 }
 
 extension HomeViewController: UICollectionViewDelegateFlowLayout {
     // MARK: DataSource
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 0
+        return 1
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return 4
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        
+        cell.backgroundColor = .white
         return cell
     }
     
     // MARK: DataSource
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
 }
