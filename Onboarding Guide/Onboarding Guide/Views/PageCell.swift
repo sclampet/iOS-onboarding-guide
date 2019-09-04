0887d4c02ce952778f1b6703cfcaee1ea5a4c096
Scott Clampet <scottclampet@Scotts-MacBook-Pro-4.local>
2019-09-04 14:11:46 -0600

    adjust pagecell image to be full screen

--- Onboarding Guide/Onboarding Guide/Views/PageCell.swift
+++ Onboarding Guide/Onboarding Guide/Views/PageCell.swift
@@ -51,9 +51,8 @@ class PageCell: BaseCell {
         addSubview(imageView)
         addSubview(textView)
         
+        addConstraintsWithFormat(format: "V:|[v0]|", views: imageView)
         addConstraintsWithFormat(format: "H:|[v0]|", views: imageView)
-        imageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        imageView.bottomAnchor.constraint(equalTo: textView.topAnchor).isActive = true
         
         addConstraintsWithFormat(format: "H:|-20-[v0]-20-|", views: textView)
         textView.heightAnchor.constraint(equalToConstant: 250).isActive = true
